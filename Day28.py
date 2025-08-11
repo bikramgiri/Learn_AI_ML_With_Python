4e04d53ab6f7bc79269fe2687aa604aa1d4c8e5a
bikramgiri <asgbrg.github@gmail.com>
2025-08-11 21:31:55 +0545

    Day28

---
+++ Day28.py
@@ -0,0 +1,7 @@
+# Day28: Breast Cancer Dataset
+
+# In Dataframes and Series: Table like Student table;
+# Column gives Attributes/Characteristics
+# Row gives Instances/Records of a particular entity/student
+
+# Missing Data: Either we assume it or we completely remove it
