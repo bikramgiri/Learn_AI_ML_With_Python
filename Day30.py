986657993fbed96fb51b05d6788ee5ad75c07273
bikramgiri <asgbrg.github@gmail.com>
2025-08-15 00:21:25 +0545

    Data cleaning by using filtering and sorting

---
+++ Day30.py
@@ -0,0 +1,17 @@
+# Day30: Filtering and Sorting
+
+# Filtering: In short, it means fetching the necessary data only at a specific time.
+# The process of fetching the necessary data only at a specific time is known as filtering.
+# df[df['Total_Marks'] > 80]
+
+# Sorting: In short, it means arranging data in a particular format either in ascending or descending or both order. 
+# Arranging data in a particular format either in ascending or descending or both order.
+# df.sort_values(by='Total_Marks', ascending=False, inplace=True)
+
+# sort_index: 
+# It means sorting the DataFrame by its index.
+# df.sort_index(inplace=True)
+
+# reset_index
+# It means resetting the index of the DataFrame.
+# df.reset_index(drop=True,inplace=True)
