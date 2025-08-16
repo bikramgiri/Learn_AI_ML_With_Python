296ac2806957cd4f04d5badeb8081fced8897b60
bikramgiri <asgbrg.github@gmail.com>
2025-08-16 21:08:31 +0545

    Basic Mathematical Operations in Pandas

---
+++ Day31.py
@@ -0,0 +1,9 @@
+# Day31: Basic Mathematical Operations in pandas: Basic Arithmetic Operations, Statistics, Element Wise Operations, Applying Functions, Row Wise and Column Wise Operations
+
+
+
+# Row Wise and Column Wise Operations
+
+# While doing col - sum:
+# 1. Unknown indices
+# 2. We need to be careful about NaN values
