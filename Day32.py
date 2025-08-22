883933ca5b7e9d2e9d3d9a393fb78f19e9ba92ca
bikramgiri <asgbrg.github@gmail.com>
2025-08-22 21:01:52 +0545

    Data Cleaning Basics using CSV Data Set with functional pipeline

---
+++ Day32.py
@@ -0,0 +1,11 @@
+#Day32: Data Cleaning
+# Data Set: day1_data.csv
+# Cleaned Data: cleaned_day1_data.csv
+
+#Day1: Basics
+#Day2: Outliers Detection and handling
+#Day3: Project on Data Cleaning
+
+#**Day1: Basics
+
+
