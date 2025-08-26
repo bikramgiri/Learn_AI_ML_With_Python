e752670b3af3422481504bbcfb3b024c4cb0989e
bikramgiri <asgbrg.github@gmail.com>
2025-08-26 21:28:03 +0545

    Data Cleaning on raw dataset.csv

---
+++ Day35.py
@@ -0,0 +1,10 @@
+# Day35: Day-4(Last Day) of Data Cleaning
+# day4_raw_dataset.csv
+# cleaned_day4_raw_dataset.csv
+
+
+# Note:
+# If in any column if the missing data covers over 50% then we discard the column.
+
+
+
