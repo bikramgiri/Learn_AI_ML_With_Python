21d1f246ff1138c6dbea033d5717b10f102aa96c
bikramgiri <asgbrg.github@gmail.com>
2025-09-19 02:02:33 +0545

    Normal Distribution

---
+++ Day46.py
@@ -0,0 +1,10 @@
+#Day46: Normal Distribution
+
+# Normal Distribution: In short, it means that the data is symmetrically distributed around the mean, with most of the data points falling close to the mean and fewer data points falling further away from the mean.
+# It is also known as Gaussian distribution and is characterized by its bell-shaped curve.
+# It is important because many statistical tests and methods assume that the data follows a normal distribution.
+# Many natural phenomena, such as height, weight, and IQ scores, are approximately normally distributed
+# The mean, median, and mode of a normal distribution are all equal and located at the center of the distribution.
+# The standard deviation of a normal distribution determines the spread of the data around the mean. 
+# In a normal distribution, approximately 68% of the data falls within one standard deviation of the mean, 95% falls within two standard deviations, and 99.7% falls within three standard deviations. This is known as the empirical rule or the 68-95-99.7 rule.
+# The normal distribution is used in many statistical tests, such as t-tests and ANOVA,
