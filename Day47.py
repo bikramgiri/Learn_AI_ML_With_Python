34b9d2df3a6442ccecc8bd74b87bd213f8cc4e7c
bikramgiri <asgbrg.github@gmail.com>
2025-09-20 23:10:06 +0545

    Day47

---
+++ Day47.py
@@ -0,0 +1,4 @@
+# Day47: Explore popular BI(BI stands for Business Intelligence) tools: Tableau for data analytics
+
+# In tableau, I explore view histogram, box plot, and line plot from two DataSet: nepali_students_dataset.csv and vgsales.csv
+
