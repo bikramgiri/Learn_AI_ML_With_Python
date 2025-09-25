5ddaa059239769fc42b28f822292e790b1b3abc6
bikramgiri <asgbrg.github@gmail.com>
2025-09-25 20:09:50 +0545

    Day49

---
+++ Day49.py
@@ -0,0 +1,20 @@
+# Day49: 
+
+
+# **Categories of Machine Learning:
+# - Based on Supervision: Unsupervised Learning, Semi-Supervised Learning, Reinforcement Learning
+
+# *Unsupervised Learning:
+# In this approach, the model is trained on unlabeled data, and it tries to find patterns or structures in the data without any explicit guidance on what the output should be.
+# E.g: Clustering, Dimensionality Reduction
+
+# *Semi-Supervised Learning:
+# In this approach, the model is trained on a small amount of labeled data and a large amount of unlabeled data. 
+# It combines the strengths of both supervised and unsupervised learning.
+# E.g: Text Classification, Image Classification with Limited Labels
+
+# *Reinforcement Learning:
+# In this approach, the model learns by interacting with an environment and receiving feedback in the form of rewards or penalties. 
+# The model aims to maximize cumulative rewards over time.
+# 
+# E.g: Game Playing (e.g., AlphaGo), Robotics, Autonomous Vehicles
