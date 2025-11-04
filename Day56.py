df8a3f5e84bbe5da1cda44353e2ab2f474c75afa
bikramgiri <asgbrg.github@gmail.com>
2025-11-04 22:26:32 +0545

    Day 56

---
+++ Day56.py
@@ -0,0 +1,27 @@
+#Day56: 
+
+
+# *Cost function of logistic regression:
+# The cost function of logistic regression is same as linear regression, but with a different hypothesis function.
+# It is used to measure how well the logistic regression model fits the data.
+# The cost function for logistic regression is mathematically sound but does not use the same optimization techniques as linear regression because of the non-linear nature of the logistic function.
+
+#*Concave function: It symbol is ∩
+# It has a single global maximum and it lies at the top.
+# It is a function where the line segment between any two points on the graph of the function lies below or on the graph.
+# It is used to measure how well the logistic regression model fits the data.# It is used to optimize the parameters of the logistic regression model during training.
+# The cost function for logistic regression is called the log-loss or logistic loss function.
+# In logistic regression, we want to minimize the cost function to find the best-fitting model.
+
+# *Convex function: It symbol is ∪
+# It has a single global minimum and it lies at the bottom.
+# It is a function where the line segment between any two points on the graph of the function lies above or on the graph.
+# It is used in optimization problems to ensure that there is a unique global minimum.
+# The cost function for logistic regression is concave, which means it has a unique global maximum.
+# It is used to measure the performance of a logistic regression model.
+# The cost function for logistic regression is convex, which means it has a unique global minimum.
+
+# Gradient Descent function is like convex function
+# It is used to find the minimum of the cost function.
+# It works by iteratively updating the model parameters in the direction of the negative gradient of the cost function.
+# This ensures that the algorithm converges to the global minimum.
