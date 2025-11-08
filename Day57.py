f1c51a516e93134e027de3e5929dd1ee8a4ef2d0
bikramgiri <asgbrg.github@gmail.com>
2025-11-08 14:55:03 +0545

    Day 57

---
+++ Day57.py
@@ -0,0 +1,8 @@
+# Day57: 
+
+# *The convergence Algorithm
+# Repeat until the convergence
+
+# In Gradient Descent graph: Graph is like ∪
+# when tangent line right side point to the downward/bottom then slope is negative
+# and when tangent line right side point to the upward/top then slope is positive
